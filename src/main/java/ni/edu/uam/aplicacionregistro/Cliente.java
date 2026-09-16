501f013316fefd7d38f7efd699df25c23fc09c7a
gabi122007 <gabiespr07@gmail.com>
2026-09-16 16:22:11 -0600

    refactor: eliminar getters y setters manuales y reemplazar con Lombok

--- src/main/java/ni/edu/uam/aplicacionregistro/Cliente.java
+++ src/main/java/ni/edu/uam/aplicacionregistro/Cliente.java
@@ -1,8 +1,15 @@
 package ni.edu.uam.aplicacionregistro;
 
+import lombok.*;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Cliente {
     private String nombres;
     private String apellidos;
@@ -13,31 +20,5 @@ public class Cliente {
     private List<String> serviciosInteres;
     private String rutaFoto;
 
-    public Cliente(String nombres, String apellidos, String tipoCliente, String ciudad,
-                   LocalDate fechaNacimiento, String tipoSolicitud,
-                   List<String> serviciosInteres, String rutaFoto) {
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.tipoCliente = tipoCliente;
-        this.ciudad = ciudad;
-        this.fechaNacimiento = fechaNacimiento;
-        this.tipoSolicitud = tipoSolicitud;
-        this.serviciosInteres = serviciosInteres;
-        this.rutaFoto = rutaFoto;
-    }
 
-    public String getNombres() { return nombres; }
-    public String getApellidos() { return apellidos; }
-    public String getNombreCompleto() { return nombres + " " + apellidos; }
-    public String getTipoCliente() { return tipoCliente; }
-    public String getCiudad() { return ciudad; }
-    public LocalDate getFechaNacimiento() { return fechaNacimiento; }
-    public String getTipoSolicitud() { return tipoSolicitud; }
-    public List<String> getServiciosInteres() { return serviciosInteres; }
-    public String getRutaFoto() { return rutaFoto; }
-
-    @Override
-    public String toString() {
-        return getNombreCompleto();
-    }
 }
